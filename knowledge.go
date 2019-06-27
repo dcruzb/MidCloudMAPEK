342f73443288d146b98573da2c28c381a993565f
Daniel Brandão <dcb@cin.ufpe.br>
2019-06-27 01:56:54 -0300

    Implementadas chamadas para o proxy de cloud functions

--- knowledge.go
+++ knowledge.go
@@ -1,6 +1,9 @@
 package MidCloudMAPEK
 
-import "github.com/dcbCIn/MidCloud/services/common"
+import (
+	"github.com/dcbCIn/MidCloud/lib"
+	"github.com/dcbCIn/MidCloud/services/common"
+)
 
 type CloudService struct {
 	Aor     common.NamingRecord
@@ -13,9 +16,31 @@ type CloudService struct {
 // Calls each of the cloud services using a generic proxy to get data to make the Rank based on a predefined sort method
 func (cs *CloudService) RefreshPrice() {
 	// Todo implement call to each of the cloud services using a generic proxy to get current price
+	cfp := *NewCloudFunctionsProxy(cs.Aor.ClientProxy.Ip, cs.Aor.ClientProxy.Port, cs.Aor.ClientProxy.ObjectId)
+	price, err := cfp.Price(1024)
+	if err != nil {
+		lib.PrintlnError("Error while getting price from", cs.Aor.ServiceName, ". Error:", err)
+	}
+	err = cfp.Close()
+	if err != nil {
+		lib.PrintlnError("Error while closing CloudFunctionsProxy.Price from", cs.Aor.ServiceName, ". Error:", err)
+	}
+
+	cs.Price = price
 }
 
 // Calls each of the cloud services using a generic proxy to get status of them to make the Rank based on a predefined sort method
 func (cs *CloudService) RefreshStatus() {
 	// Todo implement call to each of the cloud services using a generic proxy to get status
+	cfp := *NewCloudFunctionsProxy(cs.Aor.ClientProxy.Ip, cs.Aor.ClientProxy.Port, cs.Aor.ClientProxy.ObjectId)
+	status, err := cfp.Availability()
+	if err != nil {
+		lib.PrintlnError("Error while getting availability from", cs.Aor.ServiceName, ". Error:", err)
+	}
+	err = cfp.Close()
+	if err != nil {
+		lib.PrintlnError("Error while closing CloudFunctionsProxy.Status from", cs.Aor.ServiceName, ". Error:", err)
+	}
+
+	cs.Status = status
 }
