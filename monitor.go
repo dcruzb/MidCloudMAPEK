b176ce9b0066c89408ccceb32c007c26c631d82f
Daniel Brandão <dcb@cin.ufpe.br>
2019-06-28 14:48:21 -0300

    Lookup passado para dentro da função pois não é necessário controle externo da conexão

--- monitor.go
+++ monitor.go
@@ -11,7 +11,7 @@ type Monitor struct {
 	cloudServices []CloudService
 	//nameServerIp          string
 	//nameServerPort        int
-	lookup dist.LookupProxy
+	//lookup dist.LookupProxy
 	//	cloudFunctionName     string
 	cloudFunctionsPattern string
 }
@@ -19,24 +19,22 @@ type Monitor struct {
 // Starts monitoring the clouds available and caches the rank of their prices
 //
 // lookupProxy its the NameServer that will be used for the monitoring
-// cloudFunctionName stands for the name that will be binded in the nameServer
 // cloudFunctionsPattern stands for de pattern of the services that will be monitored. Any service that contains this pattern in the Lookup Proxy will be monitored
 //
-//	go monitor(lp, "cloudFunctions", "CloudFunctions")
+//	go monitor("localhost", 45000, "CloudFunctions", chanAnalyzer)
 func (mon Monitor) Start(ip string, port int, cloudFunctionsPattern string, chanAnalyzer chan []CloudService) {
 
-	//mon.cloudFunctionName = cloudFunctionName
 	mon.cloudFunctionsPattern = cloudFunctionsPattern
 
 	for {
-		mon.lookup = *dist.NewLookupProxy(ip, port)
+		//mon.lookup = *dist.NewLookupProxy(ip, port)
 
-		mon.refreshCloudServices()
+		mon.refreshCloudServices(ip, port)
 
-		err := mon.lookup.Close()
-		if err != nil {
-			lib.PrintlnError("Error at closing lookup. Error:", err)
-		}
+		//err := mon.lookup.Close()
+		//if err != nil {
+		//	lib.PrintlnError("Error at closing lookup. Error:", err)
+		//}
 
 		for i := range mon.cloudServices {
 			mon.cloudServices[i].RefreshPrice()
@@ -52,18 +50,17 @@ func (mon Monitor) Start(ip string, port int, cloudFunctionsPattern string, chan
 }
 
 // Get the list of cloud services based on name server list of binded servers
-func (mon *Monitor) refreshCloudServices() {
-	//lp := dist.NewLookupProxy(mon.nameServerIp, mon.nameServerPort)
-	services, err := mon.lookup.List()
+func (mon *Monitor) refreshCloudServices(ip string, port int) {
+	lp := dist.NewLookupProxy(ip, port)
+	services, err := lp.List()
 	if err != nil {
 		lib.PrintlnError("Error at lookup. Error:", err)
 	}
 
-	// Todo monitor não está fechando e reabrindo a conexão para o lookup, pois é acessado diversas vezes depois (a cada intervalo de tempo pré-definido). Isso ocasiona a quebra do sistema de monitoramento caso o servidor de nomes seja reiniciado.
-	//err = mon.lookup.Close()
-	//if err != nil {
-	//	lib.PrintlnError("Error at closing lookup. Error:", err)
-	//}
+	err = lp.Close()
+	if err != nil {
+		lib.PrintlnError("Error at closing lookup. Error:", err)
+	}
 
 	for _, cloudService := range mon.cloudServices {
 		cloudService.Removed = true
